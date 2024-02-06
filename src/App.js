e3f99043353e090f1bb1106142e1d159a9dc0c14
Ashakuur1 <engshakrayare114@gmail.com>
2024-02-06 22:51:39 +0300

    I added Register & Login components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Login from "./pages/Login";
 // import ProductList from "./pages/ProductList";
 // import Home from "./pages/Home";
-import Product from "./pages/Product";
+// import Product from "./pages/Product";
+// import Register from "./pages/Register";
 
 function App() {
-  return <Product />;
+  return <Login />;
 }
 
 export default App;
